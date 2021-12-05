55a150b563ac2a1efa456dcb12721fcbf6b14d61
AnthonyZ <1119886232@qq.com>
2021-12-05 18:49:36 +0800

    insertBefore has been done!

--- 实验6/2021NKUCS-Compilers-Lab-lab6/src/BasicBlock.cpp
+++ 实验6/2021NKUCS-Compilers-Lab-lab6/src/BasicBlock.cpp
@@ -20,7 +20,10 @@ void BasicBlock::insertBack(Instruction *inst)
 void BasicBlock::insertBefore(Instruction *dst, Instruction *src)
 {
     // Todo
-
+    src->getPrev()->setNext(dst);
+    dst->setPrev(src->getPrev());
+    dst->setNext(src);
+    src->setPrev(dst);
     dst->setParent(this);
 }
 
