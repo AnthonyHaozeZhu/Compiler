87bbb3f4519473c49535dd62f2184e5243936b09
AnthonyZ <1119886232@qq.com>
2021-12-06 11:25:40 +0800

    添加了日志打印

--- 实验6/2021NKUCS-Compilers-Lab-lab6/src/Function.cpp
+++ 实验6/2021NKUCS-Compilers-Lab-lab6/src/Function.cpp
@@ -32,21 +32,25 @@ void Function::output() const
     FunctionType* funcType = dynamic_cast<FunctionType*>(sym_ptr->getType());
     Type *retType = funcType->getRetType();
     fprintf(yyout, "define %s %s() {\n", retType->toStr().c_str(), sym_ptr->toStr().c_str());
+//    std::cout << "执行了Function的define" << std::endl;
     std::set<BasicBlock *> v;
     std::list<BasicBlock *> q;
     q.push_back(entry);
     v.insert(entry);
     while (!q.empty())
     {
+//        std::cout << "进入了Function的while循环" << std::endl;
         auto bb = q.front();
         q.pop_front();
-        bb->output();
+        bb->output();//出现问题的地方
         for (auto succ = bb->succ_begin(); succ != bb->succ_end(); succ++)
         {
+//            std::cout << "进入了function的for循环" << std::endl;
             if (v.find(*succ) == v.end())
             {
                 v.insert(*succ);
                 q.push_back(*succ);
+//                std::cout << "function的if判断完毕" << std::endl;
             }
         }
     }
